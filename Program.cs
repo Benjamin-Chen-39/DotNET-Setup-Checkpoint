0c79acdf0b69e54bacbfb5bd56e14dabc4d10582
Benjamin Chen <Benjamin.Chen@lntinfotech.com>
2021-09-20 11:22:11 -0400

    Changed Hello World text

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNET_Setup_Checkpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Benjamin Chen!");
+        }
+    }
+}
